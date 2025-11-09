806a777a9b60ee592f0c389b6809189d34cd7d02
chpxu <dev.chpxu@outlook.com>
2025-11-09 14:32:33 +0000

    vesta: revert back to stdenv.mkDerivation.

--- pkgs/VESTA/default.nix
+++ pkgs/VESTA/default.nix
@@ -18,8 +18,8 @@
     inherit url;
     sha256 = "sha256-WvO+Rc0Z1LYBudPhkNOf2M9m8BPgEkIIOYR7tFGaM6Y=";
   };
-
-  vestaDeriv = pkgs.stdenv.mkDerivation rec {
+in
+  pkgs.stdenv.mkDerivation rec {
     inherit pname src;
     version = "gtk3-x86_64";
     buildInputs = [gtk3 gtk2 cairo libcxx];
@@ -35,7 +35,6 @@
       pkg-config
       pkgs25.webkitgtk_4_0
       pkgs.libglvnd
-      pkgs.mesa.drivers
     ];
     LD_LIBRARY_PATH = with pkgs;
       lib.makeLibraryPath [
@@ -47,6 +46,8 @@
         xorg.libX11
         libGLU
         libglvnd
+        glfw
+        libdrm
       ];
 
     unpackPhase = ''
@@ -60,50 +61,18 @@
       # VESTA is downloaded with the executable
       # and all the other files needed, so we don't want
       # to lose any of them
-      cp -r "${pname}-${version}"/* "$out/share/applications/$pname"
-      ln -s "$out/share/applications/$pname/$pname" "$out/bin/$pname"
-      ln -s "$out/share/applications/$pname/${pname}-gui" "$out/bin/${pname}-gui"
-      #install -m755 -D VESTA $out/bin/VESTA
-
-      # install -m 444 -D desktopEntry/share/applications/${pname}.desktop $out/share/applications/${pname}.desktop
-      # cp desktopEntry/share/applications/${pname}.desktop $out/share/applications/${pname}.desktop
-      # ln -s $out/share/applications/$pname/img/logo@2x.png  $out/share/icons
+      cp -r "${pname}-${version}"/* "$out/bin"
+      cat <<INI > $out/share/applications/${pname}.desktop
+      [Desktop Entry]
+      Terminal=false
+      Name=${pname}
+      Exec=$out/bin/${pname} %f
+      Type=Application
+      icon = "$out/bin/img/logo@2x.png"
+      categories = ["Science"]
+      comment = "VESTA is a 3D visualization program for structural models, volumetric data such as electron/nuclear densities, and crystal morphologies."
+      INI
     '';
-    # postFixup = ''
-    #   patchelf \
-    #     --add-needed ${pkgs.libGL}/lib/libGL.so.1 \
-    #     $out/share/applications/$pname/VESTA-gui
-    # '';
-  };
-in
-  pkgs.buildFHSEnv rec {
-    name = pname;
-    targetPkgs = pkgs:
-      with pkgs; [
-        vestaDeriv
-        gtk3
-        gtk2
-        cairo
-        libcxx
-        libGL
-        libGLU
-      ];
-    runScript = "${vestaDeriv}/share/applications/VESTA/VESTA-gui";
-    wrappedVesta = pkgs.writeShellScriptBin "VESTA" ''
-      export LD_LIBRARY_PATH = "/run/opengl-driver/lib:/run/opengl-driver-32/lib:$LD_LIBRARY_PATH"
-      cd ${vestaDeriv}/share/applications/VESTA/
-      exec ./VESTA-gui "$@"
-    '';
-    desktopEntry = pkgs.makeDesktopItem {
-      name = pname;
-      desktopName = pname;
-      exec = "VESTA";
-      terminal = true;
-      icon = "${vestaDeriv}/share/applications/VESTA/img/logo@2x";
-      categories = ["Science"];
-      comment = "VESTA is a 3D visualization program for structural models, volumetric data such as electron/nuclear densities, and crystal morphologies.";
-      type = "Application";
-    };
     meta = {
       description = "VESTA";
       homepage = "https://jp-minerals.org/vesta/en/";
